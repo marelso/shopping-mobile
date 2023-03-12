d66c8bf2dada3b19a3216f99d0fed45f09918300
Marcelo Klein <marcelo.klein@townsq.com.br>
2023-03-12 16:33:29 -0300

    Include new text field to include some text and a button to apply the typed text into the text view

--- app/src/main/java/com/example/shoppingmobile/MainActivity.kt
+++ app/src/main/java/com/example/shoppingmobile/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.shoppingmobile
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+    fun submit(view: View) {
+        val inputField = findViewById<EditText>(R.id.inputField)
+        val textView = findViewById<TextView>(R.id.textView)
+        val text = inputField.text.toString()
+        textView.text = text
+    }
+
 }
