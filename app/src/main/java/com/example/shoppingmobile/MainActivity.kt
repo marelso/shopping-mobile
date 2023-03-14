040249ebb43e88520d5c69ae06cba141f94e9ba4
Marcelo Klein <marcelo.klein@townsq.com.br>
2023-03-14 09:09:11 -0300

    End of development of input screen.

--- app/src/main/java/com/example/shoppingmobile/MainActivity.kt
+++ app/src/main/java/com/example/shoppingmobile/MainActivity.kt
@@ -2,20 +2,32 @@ package com.example.shoppingmobile
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
+import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import androidx.appcompat.widget.Toolbar
 
 class MainActivity : AppCompatActivity() {
+
+    private var toolbar: Toolbar? = null
+    private var inputField: EditText? = null
+    private var submitButton: Button? = null
+    private var textView: TextView? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
-    fun submit(view: View) {
-        val inputField = findViewById<EditText>(R.id.inputField)
-        val textView = findViewById<TextView>(R.id.textView)
-        val text = inputField.text.toString()
-        textView.text = text
-    }
 
+        toolbar = findViewById(R.id.toolbar)
+        inputField = findViewById(R.id.inputField)
+        submitButton = findViewById(R.id.submitButton)
+        textView = findViewById(R.id.textView)
+
+        submitButton?.setOnClickListener {
+            textView?.text = inputField?.text.toString()
+        }
+
+        toolbar?.title = "Shopping Mobile"
+        setSupportActionBar(toolbar)
+    }
 }
