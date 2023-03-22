579ced7e1914ac965fabfa9853697ed3aa8f7958
Marcelo Klein <marcelo.klein@townsq.com.br>
2023-03-22 13:29:53 -0300

    Including bottom nav in admin fragment.

--- app/src/main/java/com/example/shoppingmobile/admin/AdminFragment.kt
+++ app/src/main/java/com/example/shoppingmobile/admin/AdminFragment.kt
@@ -1,7 +1,17 @@
 package com.example.shoppingmobile.admin
 
+import android.os.Bundle
+import android.view.*
+import android.widget.Switch
 import androidx.fragment.app.Fragment
 import com.example.shoppingmobile.R
+import com.example.shoppingmobile.basics.BasicsFragment
+import com.example.shoppingmobile.catalog.CatalogFragment
+import com.example.shoppingmobile.category.CategoryFragment
+import com.example.shoppingmobile.coupon.CouponFragment
+import com.example.shoppingmobile.offer.OfferFragment
+import com.example.shoppingmobile.user.UserFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 /**
  * A simple [Fragment] subclass.
@@ -10,4 +20,51 @@ import com.example.shoppingmobile.R
  */
 class AdminFragment : Fragment(R.layout.fragment_admin) {
 
+    private lateinit var bottomNavigationView: BottomNavigationView
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        switchViewMode(CatalogFragment())
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        bottomNavigationView = view.findViewById(R.id.bottom_navigation)
+
+        bottomNavigationView.setOnNavigationItemSelectedListener {
+            item -> switchMenuItem(item)
+        }
+    }
+
+    private fun switchViewMode(fragment: Fragment) {
+        requireActivity().supportFragmentManager.beginTransaction()
+            .replace(R.id.flManage, fragment)
+            .commit()
+    }
+
+    private fun switchMenuItem(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.catalogs -> {
+                switchViewMode(CatalogFragment())
+                return true
+            }
+            R.id.categories -> {
+                switchViewMode(CategoryFragment())
+                return true
+            }
+            R.id.offers -> {
+                switchViewMode(OfferFragment())
+                return true
+            }
+            R.id.coupons -> {
+                switchViewMode(CouponFragment())
+                return true
+            }
+            R.id.basics -> {
+                switchViewMode(BasicsFragment())
+                return true
+            }
+            else -> return false
+        }
+    }
 }
