108802b6cc5c59eaefc087505145a890104f1f0d
Marcelo Klein <marcelo.klein@townsq.com.br>
2023-04-01 20:37:30 -0300

    Including updating and deleting methods

--- app/src/main/java/com/example/shoppingmobile/domain/CatalogAdapter.kt
+++ app/src/main/java/com/example/shoppingmobile/domain/CatalogAdapter.kt
@@ -1,21 +1,26 @@
 package com.example.shoppingmobile.domain
 
-import android.annotation.SuppressLint
 import android.content.Context
-import android.content.DialogInterface
-import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.EditText
-import android.widget.LinearLayout
+import android.widget.ImageButton
 import android.widget.TextView
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.example.shoppingmobile.R
+import com.example.shoppingmobile.service.ApiClient
+import com.example.shoppingmobile.service.CatalogService
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import org.w3c.dom.Text
+
 
 class CatalogAdapter : ListAdapter<Catalog, CatalogAdapter.CatalogViewHolder>(CatalogDiffCallback()) {
 
@@ -29,25 +34,53 @@ class CatalogAdapter : ListAdapter<Catalog, CatalogAdapter.CatalogViewHolder>(Ca
         holder.bind(catalog)
     }
 
-    class CatalogViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    inner class CatalogViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        var service = ApiClient.createService(CatalogService::class.java)
+
+        var textCatalogId: TextView = itemView.findViewById<TextView>(R.id.textCatalogId)
+        var textCatalogName: TextView = itemView.findViewById<TextView>(R.id.textCatalogName)
+        var textCatalogDescription: TextView = itemView.findViewById<TextView>(R.id.textCatalogDescription)
 
         fun bind(catalog: Catalog) {
-
-            itemView.findViewById<TextView>(R.id.textCatalogId).text = catalog.id.toString()
-            itemView.findViewById<TextView>(R.id.textCatalogName).text = catalog.name
-            itemView.findViewById<TextView>(R.id.textCatalogDescription).text = catalog.description
+            textCatalogId.text = catalog.id.toString()
+            textCatalogName.text = catalog.name
+            textCatalogDescription.text = catalog.description
 
             itemView.findViewById<Button>(R.id.btnView).setOnClickListener {
                 viewDetailsClick(itemView.context,catalog)
+            }
+
+            itemView.findViewById<ImageButton>(R.id.btnDelete).setOnClickListener {
+                CoroutineScope(Dispatchers.Main).launch {
+                    val response = service.delete(catalog.id)
+
+                    withContext(Dispatchers.Main) {
+                        if (response.isSuccessful) {
+                            val position = currentList.indexOf(catalog)
+                            if (position != -1) {
+                                val newList = currentList.toMutableList()
+                                newList.removeAt(position)
+                                submitList(newList)
+                            }
+                            println("OK")
+                        } else {
+                            println("error")
+                        }
+                    }
+                }
             }
         }
 
         fun viewDetailsClick(context: Context, catalog: Catalog) {
             val dialogView = LayoutInflater.from(context).inflate(R.layout.catalog_dialog, null)
 
-            dialogView.findViewById<TextView>(R.id.textCatalogId).text = catalog.id.toString()
-            dialogView.findViewById<EditText>(R.id.textCatalogName).setText(catalog.name)
-            dialogView.findViewById<EditText>(R.id.textCatalogDescription).setText(catalog.description)
+            var textId: TextView = dialogView.findViewById<TextView>(R.id.textCatalogId)
+            var textUpdatedName: EditText = dialogView.findViewById<EditText>(R.id.textCatalogName)
+            var textUpdatedDescription: EditText = dialogView.findViewById<EditText>(R.id.textCatalogDescription)
+
+            textId.setText(catalog.id.toString())
+            textUpdatedName.setText(catalog.name)
+            textUpdatedDescription.setText(catalog.description)
 
             val dialog = MaterialAlertDialogBuilder(context, R.style.Theme_ShoppingMobile)
                 .setTitle("Are you absolutely sure?")
@@ -55,6 +88,21 @@ class CatalogAdapter : ListAdapter<Catalog, CatalogAdapter.CatalogViewHolder>(Ca
                 .setView(dialogView)
                 .setPositiveButton("Update") { dialog, which ->
                     println("Update")
+
+                    CoroutineScope(Dispatchers.Main).launch {
+                        var updatedCatalog: Catalog = Catalog(catalog.id, textUpdatedName.text.toString(), textUpdatedDescription.text.toString())
+                        val response = service.update(catalog.id, updatedCatalog)
+
+                        withContext(Dispatchers.Main) {
+                            if (response.isSuccessful) {
+                                textCatalogName.setText(updatedCatalog.name)
+                                textCatalogDescription.setText(updatedCatalog.description)
+                                println("OK")
+                            } else {
+                                println("error")
+                            }
+                        }
+                    }
                 }
                 .setNegativeButton("Cancel") { dialog, which ->
                     println("Cancel")
@@ -64,7 +112,6 @@ class CatalogAdapter : ListAdapter<Catalog, CatalogAdapter.CatalogViewHolder>(Ca
             dialog.window?.setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT)
 
             dialog.show()
-
         }
     }
 
--- app/src/main/java/com/example/shoppingmobile/service/CatalogService.kt
+++ app/src/main/java/com/example/shoppingmobile/service/CatalogService.kt
@@ -1,7 +1,10 @@
 package com.example.shoppingmobile.service
 
 import com.example.shoppingmobile.domain.Catalog
+import okhttp3.ResponseBody
 import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 import retrofit2.http.*
 
 interface CatalogService {
@@ -11,6 +14,9 @@ interface CatalogService {
     @POST("catalogs")
     suspend fun create(@Body request: Catalog): Catalog
 
+    @PUT("catalogs/{id}")
+    suspend fun update(@Path("id") id: Int, @Body request: Catalog): Response<ResponseBody>
+
     @DELETE("catalogs/{id}")
-    suspend fun delete(@Path("id") id: Int): Call<Unit>
+    suspend fun delete(@Path("id") id: Int): Response<ResponseBody>
 }
