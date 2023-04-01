5ab22b6ca785b1f29efad641f14ab4971453099d
Marcelo Klein <marcelo.klein@townsq.com.br>
2023-04-01 12:47:29 -0300

    Including material dialogs

--- app/src/main/java/com/example/shoppingmobile/domain/CatalogAdapter.kt
+++ app/src/main/java/com/example/shoppingmobile/domain/CatalogAdapter.kt
@@ -1,10 +1,15 @@
 package com.example.shoppingmobile.domain
 
+import android.annotation.SuppressLint
 import android.content.Context
+import android.content.DialogInterface
+import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.EditText
+import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
@@ -33,28 +38,33 @@ class CatalogAdapter : ListAdapter<Catalog, CatalogAdapter.CatalogViewHolder>(Ca
             itemView.findViewById<TextView>(R.id.textCatalogDescription).text = catalog.description
 
             itemView.findViewById<Button>(R.id.btnView).setOnClickListener {
-                dialogClick(itemView.context,catalog)
+                viewDetailsClick(itemView.context,catalog)
             }
         }
 
-        fun dialogClick(context: Context, catalog: Catalog) {
+        fun viewDetailsClick(context: Context, catalog: Catalog) {
             val dialogView = LayoutInflater.from(context).inflate(R.layout.catalog_dialog, null)
+
+            dialogView.findViewById<TextView>(R.id.textCatalogId).text = catalog.id.toString()
+            dialogView.findViewById<EditText>(R.id.textCatalogName).setText(catalog.name)
+            dialogView.findViewById<EditText>(R.id.textCatalogDescription).setText(catalog.description)
 
             val dialog = MaterialAlertDialogBuilder(context, R.style.Theme_ShoppingMobile)
                 .setTitle("Are you absolutely sure?")
-                .setMessage("This action cannot be undone. This will update the ${catalog.name} category.")
+                .setMessage("This action cannot be undone. This will update the ${catalog.name} catalog.")
                 .setView(dialogView)
-                .setPositiveButton("OK") { dialog, which ->
-                    // Handle button click
+                .setPositiveButton("Update") { dialog, which ->
+                    println("Update")
                 }
                 .setNegativeButton("Cancel") { dialog, which ->
-                    // Handle button click
+                    println("Cancel")
                 }
                 .create()
 
-            dialog.window?.setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT)
+            dialog.window?.setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT)
 
             dialog.show()
+
         }
     }
 
