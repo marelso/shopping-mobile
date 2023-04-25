49d284636b26d00820bd61fe5e06dba2919b8d5b
Marcelo Klein <marcelo.klein@townsq.com.br>
2023-04-25 18:08:40 -0300

    Include new entity and service to handle offer requests.

---
+++ app/src/main/java/com/example/shoppingmobile/service/OfferService.kt
@@ -0,0 +1,23 @@
+package com.example.shoppingmobile.service
+
+import com.example.shoppingmobile.domain.offer.Offer
+import okhttp3.ResponseBody
+import retrofit2.Call
+import retrofit2.http.*
+
+interface OfferService {
+    @GET("offers")
+    fun get(): Call<List<Offer>>
+
+    @GET("offers/category/{id}")
+    fun getByCategory(@Path("id") id: Int): Call<List<Offer>>
+
+    @POST("offers")
+    fun create(@Body request: Offer): Call<Offer>
+
+    @PUT("offers/{id}")
+    fun update(@Path("id") id: Int, @Body request: Offer): Call<ResponseBody>
+
+    @DELETE("offers/{id}")
+    fun delete(@Path("id") id: Int): Call<ResponseBody>
+}
