6f3924886c354340785817e77719f143650e88f1
Marcelo Klein <marcelo.klein@townsq.com.br>
2023-04-25 16:02:24 -0300

    Include new views code files.

--- app/src/main/java/com/example/shoppingmobile/user/UserFragment.kt
+++ app/src/main/java/com/example/shoppingmobile/user/UserFragment.kt
@@ -4,19 +4,21 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.shoppingmobile.R
+import com.example.shoppingmobile.catalog.CatalogFragment
 import com.example.shoppingmobile.domain.CatalogUserAdapter
 import com.example.shoppingmobile.service.ApiClient
 import com.example.shoppingmobile.service.CatalogService
 import kotlinx.coroutines.launch
 
 class UserFragment : Fragment(R.layout.fragment_user) {
-    private lateinit var catalogsRecyclerView: RecyclerView
+    private var catalogsRecyclerView: RecyclerView? = null
     private var adapter: CatalogUserAdapter? = null
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
@@ -27,25 +29,35 @@ class UserFragment : Fragment(R.layout.fragment_user) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        adapter = CatalogUserAdapter()
+
+        adapter = CatalogUserAdapter(View.OnClickListener { view ->
+            val id = Integer.valueOf(view.findViewById<TextView>(R.id.catalogIdTextView)?.text.toString())
+            val fragment = ViewCatalogFragment.newInstance(id)
+            switchViewMode(fragment)
+        })
 
         // Create the API service
         val service = ApiClient.createService(CatalogService::class.java)
 
-        catalogsRecyclerView.layoutManager = LinearLayoutManager(requireContext())
+        catalogsRecyclerView?.layoutManager = LinearLayoutManager(requireContext())
+        catalogsRecyclerView?.adapter = adapter
 
+        println(1)
         lifecycleScope.launch {
             try {
                 val catalogs = service.getCatalogs()
-                adapter?.submitList(catalogs)
-                catalogsRecyclerView.adapter = adapter
-
-                // Call notifyDataSetChanged() to update the view
+                adapter?.setData(catalogs.toMutableList())
                 adapter?.notifyDataSetChanged()
             } catch (e: Exception) {
-                // Handle the error
                 Toast.makeText(context, "Error loading catalogs: ${e.message}", Toast.LENGTH_SHORT).show()
             }
         }
+    }
+
+    private fun switchViewMode(fragment: Fragment) {
+        requireActivity().supportFragmentManager.beginTransaction()
+            .replace(R.id.flFragment, fragment)
+            .addToBackStack(null)
+            .commit()
     }
 }
--- app/src/main/java/com/example/shoppingmobile/user/ViewCatalogFragment.kt
+++ app/src/main/java/com/example/shoppingmobile/user/ViewCatalogFragment.kt
@@ -1,32 +1,39 @@
 package com.example.shoppingmobile.user
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import com.example.shoppingmobile.R
+import com.example.shoppingmobile.domain.category.Category
+import com.example.shoppingmobile.domain.category.CategoryAdapter
+import com.example.shoppingmobile.service.ApiClient
+import com.example.shoppingmobile.service.CategoryService
+import com.google.android.material.chip.Chip
+import com.google.android.material.chip.ChipGroup
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+private const val ARG_CATALOG_ID = "catalogId"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [ViewCatalogFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class ViewCatalogFragment : Fragment() {
     // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private var catalogId: Int = 0
+
+    private var noCategorySelected: TextView? = null
+    private var currentCategory: TextView? = null
+    private var availableCategories: ChipGroup? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+            catalogId = it.getInt(ARG_CATALOG_ID)
         }
     }
 
@@ -38,22 +45,84 @@ class ViewCatalogFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_view_catalog, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        availableCategories = view.findViewById(R.id.availableCategories)
+        noCategorySelected = view.findViewById(R.id.textNoCategorySelect)
+        currentCategory = view.findViewById(R.id.category)
+
+        val service = ApiClient.createService(CategoryService::class.java)
+
+        val call = service.get(catalogId)
+        call.enqueue(object : Callback<List<Category>> {
+            override fun onResponse(call: Call<List<Category>>, response: Response<List<Category>>) {
+                if (response.isSuccessful) {
+                    val categories = response.body() as List<Category>
+
+                    if(categories.isEmpty()) {
+                        noCategorySelected?.text = "No categories available at this time."
+                    }
+
+                    for (category in categories) {
+                        val chip = Chip(requireContext())
+                        chip.text = category.name
+                        chip.tag = category
+                        chip.isClickable = true
+                        chip.isCheckable = true
+                        chip.isFocusable = true
+                        chip.setOnClickListener {
+                            val selectedCategoryId = it.tag as? Category ?: return@setOnClickListener
+                            Log.d("Selected Category", category.name)
+                            currentCategory?.text = selectedCategoryId.name
+                        }
+                        availableCategories?.addView(chip)
+                    }
+                } else {
+                    //TODO handle error
+                }
+            }
+
+            override fun onFailure(call: Call<List<Category>>, t: Throwable) {
+                //TODO handle error
+            }
+        })
+
+        availableCategories?.setOnCheckedChangeListener { group, checkedId ->
+            chipCheckedChange(group, checkedId)
+        }
+    }
+
+    private fun chipCheckedChange(group: ChipGroup, checkedId: Int) {
+        if (checkedId == View.NO_ID) {
+            noCategorySelected?.visibility = View.VISIBLE
+            currentCategory?.visibility = View.INVISIBLE
+            loadOffersByCategory()
+        } else {
+            noCategorySelected?.visibility = View.GONE
+            currentCategory?.visibility = View.VISIBLE
+        }
+
+        var selectedChipId: Int? = null
+        selectedChipId?.let { group.findViewById<Chip>(it)?.isChecked = false }
+        selectedChipId = checkedId
+        selectedChipId.let { group.findViewById<Chip>(it)?.isChecked = true }
+    }
+
+    private fun loadOffersByCategory() {
+        val selectedChipId = availableCategories?.checkedChipId
+        if (selectedChipId != View.NO_ID) {
+            val selectedChip = selectedChipId?.let { view?.findViewById<Chip>(it) }
+            val category = selectedChip?.tag as Category
+        }
+    }
+
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ViewCatalogFragment.
-         */
-        // TODO: Rename and change types and number of parameters
         @JvmStatic
-        fun newInstance(param1: String, param2: String) =
+        fun newInstance(catalogId: Int) =
             ViewCatalogFragment().apply {
                 arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+                    putInt(ARG_CATALOG_ID, catalogId)
                 }
             }
     }
